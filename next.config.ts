2617b0fa543a0cf862dd6f6f5d61b737bac767a0
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-19 10:06:35 +0900

    카카오 로그인시 프로필이미지 허용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
